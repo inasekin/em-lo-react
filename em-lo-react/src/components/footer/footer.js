9077dc699005e3423a03735e83b75eab3b035145
Ivan <nasekinid8591@yandex.ru>
2021-01-22 14:44:39 +0300

    add footer

--- em-lo-react/src/components/footer/footer.js
+++ em-lo-react/src/components/footer/footer.js
@@ -1,45 +1,51 @@
 import React from 'react';
+import some_icons from '../../img/some-icons.png';
 
 const Footer = () => {
         return (
-            <footer class="footer section section--orange-bg">
-                <div class="footer__content _container">
-                    <div class="footer__site-name">Emergency Locksmith 24</div>
+            <footer class="footer section ">
+                <div class="footer__content ">
                     <div class="row">
-                        <div class="col-sm-6 d-flex flex-column justify-content-between">
-                            <div class="footer__contacts">
-                                <p><a href="#">020 7359 6070</a></p>
-                                <p><a href="">info@emergency-locks</a></p>
+                        <div class="footer-info col-lg-4 col-sm-12">
+                            <p>Emergency Locksmith 24<br/> 020 8059 5259 <br/> info@emergency-locksmith-24.co.uk</p>
+                            <div class="footer__logo">
+                                <img src={some_icons} alt="xxx" class="footer-logo__img"/>
                             </div>
-                            <div class="navbar footer__navbar">
-                                <ul class="navbar__wrapper">
-                                    <li class="navbar__item active"> <a href="#" class="navbar__link">Home</a> </li>
-                                    <li class="navbar__item"> <a href="#" class="navbar__link">Our services</a> </li>
-                                    <li class="navbar__item"> <a href="#" class="navbar__link">Locations</a> </li>
-                                    <li class="navbar__item"> <a href="#" class="navbar__link">Contact us</a> </li>
-                                </ul>
-                            </div>
+                            <p>© 2020 Emergency-locksmith-24.co.uk</p>
                         </div>
-                        <div class="col-sm-6">
-                            <div class="footer__links">
-                                <div class="row">
-                                    <div class="col-sm-6"> <a href="" class="footer__link">Emergency locksmith</a> </div>
-                                    <div class="col-sm-6"> <a href="" class="footer__link">Emergency locksmith</a> </div>
-                                    <div class="col-sm-6"> <a href="" class="footer__link">Emergency locksmith</a> </div>
-                                    <div class="col-sm-6"> <a href="" class="footer__link">Emergency locksmith</a> </div>
-                                    <div class="col-sm-6"> <a href="" class="footer__link">Emergency locksmith</a> </div>
-                                    <div class="col-sm-6"> <a href="" class="footer__link">Emergency locksmith</a> </div>
-                                    <div class="col-sm-6"> <a href="" class="footer__link">Emergency locksmith</a> </div>
-                                    <div class="col-sm-6"> <a href="" class="footer__link">Emergency locksmith</a> </div>
-                                </div>
+                        <div class="footer-adress col-lg-4 col-sm-12">
+                            <p>Our office:</p>
+                            <p>179 Queens Dr, Liverpool L15 6XS</p>
+                            <div class="sitemap-a">
+                                <a href="">Sitemap</a>
+                                <a href="">Disclaimer</a>
                             </div>
+
                         </div>
-                    </div>
-                    <div class="footer__copyright">
-                        <p>© 2020 Emergency-locksmith-24.co.uk</p>
+                        <div class="footer-services col-lg-4 col-sm-12">
+                            <ul class="row">
+                                <li class="col-6">Emergency locksmith service </li>
+                                <li class="col-6">Domestic locksmith</li>
+                                <li class="col-6">Commercial locksmith</li>
+                                <li class="col-6">Lock repair</li>
+                                <li class="col-6">Lock change</li>
+                                <li class="col-6"> Lock installation </li>
+                                <li class="col-6">Burglary repairs</li>
+                                <li class="col-6">Locked out service</li>
+                                <li class="col-6"> Key copy </li>
+                                <li class="col-6">Safe locksmith</li>
+                                <li class="col-6">Window lock repair </li>
+                                <li class="col-6">Window lock change</li>
+                                <li class="col-6"> Window lock installation </li>
+                                <li class="col-6">Garage door lock repair</li>
+                                <li class="col-6">Garage door lock change</li>
+                                <li class="col-6">Garage door lock installation</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </footer>
+
         );
     };
 export default Footer;
