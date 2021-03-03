783f59960c1e4300bd9ef133e07ab3c518717719
Ivan Nasekin <nasekin@ipavlov.ai>
2021-03-03 12:24:39 +0300

    fix console errors

--- em-lo-react/src/components/contact-block/contact-block.js
+++ em-lo-react/src/components/contact-block/contact-block.js
@@ -27,7 +27,7 @@ const ContactBlock = () => {
                         </div>
                     </div>
                     <div width="100%">
-                        <iframe scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Liverpool,%20United%20Kingdom+(Emergency%20Locksmith%2024)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="600" frameborder="0">
+                        <iframe scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Liverpool,%20United%20Kingdom+(Emergency%20Locksmith%2024)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="600" frameBorder="0">
                             </iframe>
                     </div>
                     </div>
