cfbc3328d8b684beffef103c9d9bf076be541e61
Ivan Nasekin <nasekin@ipavlov.ai>
2021-03-09 17:35:54 +0300

    add changes in sitemap page

--- em-lo-react/src/pages/Sitemap.js
+++ em-lo-react/src/pages/Sitemap.js
@@ -1,12 +1,119 @@
 import React from "react";
 import Footer from '../components/footer';
+import Banner from "../components/banner";
 
 const Sitemap = () => {
     return (
         <>
+            <Banner title="Sitemap" features1="24/7, 365 Days a Year"
+                    features2="From £ 39" features3="At your door in 20 min" telefone="020 8059 5259" />
             <div className="container">
                 <div className="sitemap-content">
-                    
+                    <a href="https://emergency-locksmith-24.co.uk/">Home</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/">Services</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/emergency-locksmith-service/">Emergency locksmith service</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/domestic-locksmith/">Domestic locksmith</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/commercial-locksmith/">commercial locksmith</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/lock-repair/">lock repair</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/lock-change/">lock change</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/lock-installation/">lock installation</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/burglary-repairs/">burglary repairs</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/locked-out-service/">locked out service</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/key-copy/">key copy</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/key-cutting/">key cutting</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/safe-locksmith/">safe locksmith</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/window-lock-repair/">window lock repair</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/window-lock-change/">window lock change</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/window-lock-installation/">window lock installation</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/garage-door-lock-repair/">garage door lock repair</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/garage-door-lock-change/">garage door lock change</a>
+                    <a href="https://emergency-locksmith-24.co.uk/services/garage-door-lock-installation/">garage door lock installation</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/">areas we cover</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-city-centre/">locksmith in city centre</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-everton/">locksmith in everton</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-vauxhall/">locksmith in vauxhall</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-anfield/">locksmith in anfield</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-kirkdale/">locksmith in kirkdale</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-walton/">locksmith in walton</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-fairfield/">locksmith in fairfield</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-kensington/">locksmith in kensington</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-tuebrook/">locksmith in tuebrook</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-edge-hill/">locksmith-in-edge-hill</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-dingle/">locksmith in dingle</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-toxteth/">locksmith in toxteth</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-aintree/">locksmith in aintree</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-fazakerley/">locksmith in fazakerley</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-orrell-park/">locksmith in orrell park</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-aintree-village/">locksmith in aintree village</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-clubmoor/">locksmith in clubmoor</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-croxteth/">locksmith in croxteth</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-gillmoss/">locksmith in gillmoss</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-norris-green/">locksmith in norris green</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-croxteth-park/">locksmith in croxteth park</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-west-derby/">locksmith in west derby</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-old-swan/">locksmith in old swan</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-stoneycroft/">locksmith in stoneycroft</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-broadgreen/">locksmith in broadgreen</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-dovecot/">locksmith in dovecot</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-knotty-ash/">locksmith in knotty ash</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-page-moss/">locksmith in page moss</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-wavertree/">locksmith in wavertree</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-bowring-park/">locksmith in bowring park</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-childwall/">locksmith in childwall</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-aigburth/">locksmith in aigburth</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-st-michael's-hamlet/">locksmith in st michael's hamlet</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-sefton-park/">locksmith in sefton park</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-allerton/">locksmith in allerton</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-mossley-hill/">locksmith in mossley hill</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-garston/">locksmith in garston</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-grassendale/">locksmith in grassendale</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-bootle/">locksmith in bootle</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-orrell/">locksmith in orrell</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-ford/">locksmith in ford</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-litherland/">locksmith in litherland</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-seaforth/">locksmith in seaforth</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-waterloo/">locksmith in waterloo</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-blundellsands/">locksmith in blundellsands</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-brighton-le-sands/">locksmith in brighton le sands</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-crosby/">locksmith in crosby</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-little-crosby/">locksmith in little crosby</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-thornton/">locksmith in thornton</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-hale/">locksmith in hale</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-speke/">locksmith in speke</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-belle-vale/">locksmith in belle vale</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-gateacre/">locksmith in gateacre</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-hunts-cross/">locksmith in hunts cross</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-woolton/">locksmith in woolton</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-halewood/">locksmith in halewood</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-netherley/">locksmith in netherley</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-stockbridge-village/">locksmith in stockbridge village</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-lunt/">locksmith in lunt</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-sefton-village/">locksmith in sefton village</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-netherton/">locksmith in netherton</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-maghull/">locksmith in maghull</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-lydiate/">locksmith in lydiate</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-melling/">locksmith in melling</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-waddicar/">locksmith in waddicar</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-kirkby/">locksmith in kirkby</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-prescot/">locksmith in prescot</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-knowsley-village/">locksmith in knowsley village</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-whiston/">locksmith in whiston</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-rainhill/">locksmith in rainhill</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-huyton/">locksmith in huyton</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-roby/">locksmith in roby</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-tarbock/">locksmith in tarbock</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-formby/">locksmith in formby</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-little-altcar/">locksmith in little altcar</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-great-altcar/">locksmith in great altcar</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-ince-blundell/">locksmith in ince blundell</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-hightown/">locksmith in hightown</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-ormskirk/">locksmith in ormskirk</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-aughton/">locksmith in aughton</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-burscough/">locksmith in burscough</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-mawdesley/">locksmith in mawdesley</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-scarisbrick/">locksmith in scarisbrick</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-rufford/">locksmith in rufford</a>
+                    <a href="https://emergency-locksmith-24.co.uk/areas-we-cover/locksmith-in-holmeswood/">locksmith in holmeswood</a>
                 </div>
             </div>
             <Footer/>
