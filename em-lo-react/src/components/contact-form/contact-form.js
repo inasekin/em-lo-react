d80f7249a59c572af7ffec6b161073b942d64878
Ivan Nasekin <nasekin@ipavlov.ai>
2021-04-14 17:42:33 +0300

    add server and fix contact form

--- em-lo-react/src/components/contact-form/contact-form.js
+++ em-lo-react/src/components/contact-form/contact-form.js
@@ -1,63 +1,132 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
 
-export default class ContactForm extends Component {
-    render() {
+const ContactForm = () => {
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
 
-        return(
-            <section className="contacts section">
-                <div className="_container">
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <div className="contacts__items">
-                                <div className="contacts__item">
-                                    <div className="contacts__title">Contact info</div>
-                                    <a className="contacts__link" href="#">info@emergency-locksmith-24.co.uk</a>
-                                    <a className="contacts__link" href="#">020 8059 5259</a>
-                                </div>
-                                <div className="contacts__item">
-                                    <div className="contacts__title">Mailing address</div>
-                                    <div className="contacts__link" href="#">Emergency Locksmith 24</div>
-                                    <div className="contacts__link" href="#">179 Queens Dr, Liverpool L15 6XS<br/>United Kingdom</div>
-                                </div>
-                                <div className="contacts__item">
-                                    <div className="contacts__title">Company Reg. Number:</div>
-                                    <div className="contacts__link" href="#">11494148</div>
-                                </div>
-                                <div className="contacts__item">
-                                    <div className="contacts__title">VAT Number:</div>
-                                    <div className="contacts__link" href="#">305-2934-37</div>
-                                </div>
+    const [result, setResult] = useState(null);
+
+    const sendEmail = event => {
+        event.preventDefault();
+        axios
+            .post('/send', { ...state })
+            .then(response => {
+                setResult(response.data);
+                setState({
+                    name: '',
+                    email: '',
+                    subject: '',
+                    message: ''
+                });
+            })
+            .catch(() => {
+                setResult({
+                    success: false,
+                    message: 'Something went wrong. Try again later'
+                });
+            });
+    };
+
+    const onInputChange = event => {
+        const { name, value } = event.target;
+
+        setState({
+            ...state,
+            [name]: value
+        });
+    };
+
+    return (
+        <section className="contacts section">
+            <div className="_container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="contacts__items">
+                            <div className="contacts__item">
+                                <div className="contacts__title">Contact info</div>
+                                <a className="contacts__link" href="#">info@emergency-locksmith-24.co.uk</a>
+                                <a className="contacts__link" href="#">020 8059 5259</a>
                             </div>
-                        </div>
-                        <div className="col-sm-6">
-                            <div className="contacts__form contact-form">
-                                <form encType="multipart/form-data" method="post" className="form" id="form" onSubmit="send(event, 'send.php')">
-                                    <div className="form__line">
-                                        <label for="name" className="input-label">Your Name (required)</label>
-                                        <input name="name" id="name" autoComplete="off" type="text" className="input contact-form__input aInput form-input aInputs"/>
-                                    </div>
-                                    <div className="form__line">
-                                        <label for="telephone">Your Phone (required)</label>
-                                        <input name="telephone" id="telephone" type="text" className="input contact-form__input aInput"/>
-                                    </div>
-                                    <div className="form__line">
-                                        <label for="email" className="input-label">Your Email</label>
-                                        <input className="input contact-form__input form-input aInput" name="email" id="email" type="text" autoComplete="off"/>
-                                    </div>
-                                    <div className="form__line">
-                                        <label for="texta" className="area-label">Your Message</label>
-                                        <textarea name="texta" id="texta" class="aInput input contact-form__input"/>
-                                    </div>
-                                    <div className="form__line">
-                                        <button type="submit" className="form__button contact-form__submit btn btn--primary">SEND</button>
-                                    </div>
-                                </form>
+                            <div className="contacts__item">
+                                <div className="contacts__title">Mailing address</div>
+                                <div className="contacts__link" href="#">Emergency Locksmith 24</div>
+                                <div className="contacts__link" href="#">179 Queens Dr, Liverpool L15 6XS<br/>United Kingdom</div>
+                            </div>
+                            <div className="contacts__item">
+                                <div className="contacts__title">Company Reg. Number:</div>
+                                <div className="contacts__link" href="#">11494148</div>
+                            </div>
+                            <div className="contacts__item">
+                                <div className="contacts__title">VAT Number:</div>
+                                <div className="contacts__link" href="#">305-2934-37</div>
                             </div>
                         </div>
                     </div>
+                    <div className="col-sm-6">
+                        <div className="contacts__form contact-form">
+                            <form onSubmit={sendEmail} encType="multipart/form-data" method="post" className="form" id="form">
+                                <Form.Group controlId="name"  className="form__line">
+                                    <Form.Label className="input-label">Full Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="name"
+                                        value={state.name}
+                                        placeholder="Enter your full name"
+                                        onChange={onInputChange}
+                                        className="input contact-form__input aInput form-input aInputs"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="email" className="form__line">
+                                    <Form.Label className="input-label">Email</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="email"
+                                        value={state.email}
+                                        placeholder="Enter your email"
+                                        onChange={onInputChange}
+                                        className="input contact-form__input form-input aInput"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="subject" className="form__line">
+                                    <Form.Label className="input-label">Subject</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="subject"
+                                        value={state.subject}
+                                        placeholder="Enter subject"
+                                        onChange={onInputChange}
+                                        className="input contact-form__input form-input aInput"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="subject" className="form__line">
+                                    <Form.Label className="input-label">Message</Form.Label>
+                                    <Form.Control
+                                        as="textarea"
+                                        name="message"
+                                        value={state.message}
+                                        rows="3"
+                                        placeholder="Enter your message"
+                                        onChange={onInputChange}
+                                        className="aInput input contact-form__input"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="subject" className="form__line">
+                                    <Button variant="primary" type="submit"  className="form__button contact-form__submit btn btn--primary">
+                                        Submit
+                                    </Button>
+                                </Form.Group>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </section>
-
-        );
-    }
-}
+            </div>
+        </section>
+    );
+};
+export default ContactForm;
